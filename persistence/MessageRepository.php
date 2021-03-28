9502cb472f70cd97e3414b6681b932026b2a27fb
pgaida <p.gaida@omikronsystems.de>
2021-03-28 20:35:01 +0200

    returning the rows directly

--- persistence/MessageRepository.php
+++ persistence/MessageRepository.php
@@ -2,7 +2,6 @@
 final class MessageRepository {
   final static function fetchAllMessages($db_connection) {
     $db_connection->prepareQuery('SELECT * FROM messages');
-    $rows = $db_connection->resultSet();
-    print_r($rows);
+    return $db_connection->resultSet();
   }
 }
