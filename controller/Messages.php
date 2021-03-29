d6330abcca8bd28269fd7018ac73a53f44899237
pgaida <p.gaida@omikronsystems.de>
2021-03-29 23:16:38 +0200

    Secured actions if not logged in

--- controller/Messages.php
+++ controller/Messages.php
@@ -2,12 +2,20 @@
 class MessagesController extends BaseController {
   
   protected function index() {
-    $view_model = new MessageModel(new MessageRepository());
-    $this->getView($view_model->index(), $view_model->isFullview());
+    if(isset($_SESSION['is_logged_in'])) {
+      $view_model = new MessageModel(new MessageRepository());
+      $this->getView($view_model->index(), $view_model->isFullview());
+    } else {
+      header("Location: " . ROOT_URL . '/users/login');
+    }
   }
 
   protected function add() {
-    $view_model = new MessageModel(new MessageRepository());
-    $this->getView($view_model->add(), $view_model->isFullview());
+    if(isset($_SESSION['is_logged_in'])) {
+      $view_model = new MessageModel(new MessageRepository());
+      $this->getView($view_model->add(), $view_model->isFullview());
+    } else {
+      header("Location: " . ROOT_URL . '/users/login');
+    }
   }
 }
